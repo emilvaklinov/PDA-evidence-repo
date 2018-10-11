2755c31dffba3cb1e77ee3a228727083fe2cd2be
trakieca <emil_vaklinov@yahoo.com>
2018-10-11 10:53:23 +0100

    task A created

--- Static_and_Dynamic_Task_A/testing_task_A.rb
+++ Static_and_Dynamic_Task_A/testing_task_A.rb
@@ -11,7 +11,9 @@ class CardGame
   end
 
   def checkforAce(card)
-    if card.value = 1
+    #method should be defined on .self
+    #best practice has method names in snake case: underscores between words, all lower case
+    if card.value = 1 #should be ==
       return true
     else
       return false
@@ -19,18 +21,28 @@ class CardGame
   end
 
   dif highest_card(card1 card2)
+  #dif is incorrect, use def
+  #method should be defined as self.
+  #comma between card1 and card2
   if card1.value > card2.value
     return card.name
+    #Class card does not have an attribute .name
   else
     card2
+    #card2 is not a complete line; should be returned
   end
 end
 end
+#remove extraneous end, as this ends class
+
 
 def self.cards_total(cards)
+  #method is outside of class
   total
+  #undefined constant total
   for card in cards
     total += card.value
     return "You have a total of" + total
+    #will return after first iteration
   end
 end
